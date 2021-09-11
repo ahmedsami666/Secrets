8d018db2676c76d35373c062d65df47c6fbfad7a
ahmedsami666 <asz123912@gmail.com>
2021-09-11 19:18:06 +0200

    level 4 salting and hashing with bcrypt

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const express = require("express");
 const ejs = require("ejs");
 const bodyPaser = require("body-parser");
 const mongoose = require("mongoose");
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
+//define number of rounds
+const saltRounds = 10;
 
 const port = 3000;
 const app = express();
@@ -30,16 +32,18 @@ app.get("/login", function(req, res){
 
 app.post("/login", function(req, res){
     const userame = req.body.username;
-    //hashing password
-    const password = md5(req.body.password);
+    const password = req.body.password;
     User.findOne({email: userame}, function(err, foundUser){
         if (err) {
             console.log(err);
         }else{
             if (foundUser){
-                if (foundUser.password === password) {
-                    res.render("secrets");
-                }
+                //check the password
+                bcrypt.compare(password, foundUser.password, function(err, result) {
+                    if (result === true) {
+                        res.render("secrets");
+                    }
+                });
             }
         }
     })
@@ -50,17 +54,20 @@ app.get("/register", function(req, res){
 });
 
 app.post("/register", function(req, res){
-    const newUser = new User({
-        email: req.body.username,
-        //hashing password
-        password: md5(req.body.password)
-    });
-    newUser.save(function(err){
-        if (!err) {
-            res.render("secrets");
-        } else {
-            res.console.log(err);
-        };
+    //salting and hashing 
+    bcrypt.hash(req.body.password, saltRounds, function(err, hash) {
+        const newUser = new User({
+            email: req.body.username,
+            //get hash value from function
+            password: hash
+        });
+        newUser.save(function(err){
+            if (!err) {
+                res.render("secrets");
+            } else {
+                res.console.log(err);
+            };
+        });
     });
 });
 
